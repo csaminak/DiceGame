9192adf05cd4163f59a39b5a633cc70c77cb684f
Samina Chhatwal <csaminak@gmail.com>
2016-07-05 15:58:09 -0400

    added roll dice click

--- dice.js
+++ dice.js
@@ -4,26 +4,31 @@ var game = {};
 
 game.timestamp = {
     element: document.getElementById('timestamp'),
-    now: new Date()
+    start: new Date()
 };
 
-game.timestamp.element.innerHTML = 'Game started ' + game.timestamp.now;
+game.timestamp.element.innerHTML = 'Game started ' + game.timestamp.start;
 
 game.rounds = [];
 
 game.dice1 = document.getElementById('dice1');
 game.dice2 = document.getElementById('dice2');
 
-function roll(event) {
-    game.dice1.innerHTML = Math.ceil((Math.random())*6); // TODO create function dry
-    game.dice2.innerHTML = Math.ceil((Math.random())*6);
-    if ( ((Number(game.dice1.innerHTML) + Number(game.dice2.innerHTML)) === 7) ||
-         ((Number(game.dice1.innerHTML) + Number(game.dice2.innerHTML)) === 11) ) {
+game.roll = function roll() {
+    var dice1 = Math.ceil((Math.random())*6);
+    var dice2 = Math.ceil((Math.random())*6);
+
+    game.dice1.innerHTML = dice1; // TODO create function dry
+    game.dice2.innerHTML = dice2;
+    if ( ((dice1 + dice2) === 7) || ((dice1 + dice2) === 11) ) {
         document.getElementById('result').innerHTML = 'Winner!';
         // document.getElementById('attempt').innerHTML = '(It took you ' + game.rounds.length + ' and ' + time + ' seconds)';
     } else {
         document.getElementById('result').innerHTML = 'Try Again';
     }
 };
- roll(event);
-// game.getElementById('RollDice').addEventListener('click', roll(event));
+
+console.log(document.getElementById('rollDice'));
+
+document.getElementById('rollDice')
+    .addEventListener('click', game.roll);
